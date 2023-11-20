699fe3a862f50e8fd25da61d6392a50081e5bcc3
凉菜 <1659488338@qq.com>
2023-11-20 17:20:21 +0800

    fix: 热更优化

--- src/plugins/hmr.ts
+++ src/plugins/hmr.ts
@@ -1,15 +1,24 @@
 import { watch } from 'chokidar'
 import {Plugin} from "@";
+import * as path from "path";
+import * as fs from "fs";
 
-const HMR=new Plugin('开发热更插件')
-const watcher=watch(process.cwd())
+const HMR=new Plugin('HMR')
+const watchDirs=[ // 只监听本地插件和内置插件的变更，模块的管不了
+    path.join(process.cwd(),'plugins'), // 本地插件
+    __dirname, // 内置插件
+]
+const watcher=watch(watchDirs.filter(p=>{
+    return fs.existsSync(p)
+}))
+
 const changeListener=(filePath:string)=>{
     const bot=HMR.bot
     const pluginFiles=bot.pluginList.map(p=>p.filePath)
-    if(pluginFiles.includes(filePath)){
+    if(watchDirs.some(dir=>filePath.startsWith(dir)) && pluginFiles.includes(filePath)){
         const plugin=bot.pluginList.find(p=>p.filePath===filePath)
         bot.logger.debug(`插件：${plugin.name} 产生变更，即将更新`)
-        if(plugin===HMR) watcher.off('change',changeListener)
+        if(plugin===HMR) watcher.close()
         const oldCache=require.cache[filePath]
         bot.unmount(plugin)
         delete require.cache[filePath]
